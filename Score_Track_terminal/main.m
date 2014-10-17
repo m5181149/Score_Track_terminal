271fd81528b694475ae09e87c2b88aabe179a225
Yuki Kamada <yuki@wlan2657.u-aizu.ac.jp>
2014-10-17 23:23:02 +0900

    Initial Commit

---
+++ Score_Track_terminal/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Score_Track_terminal
+//
+//  Created by Yuki Kamada on 2014/10/17.
+//  Copyright (c) 2014年 Yuki Kamada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
